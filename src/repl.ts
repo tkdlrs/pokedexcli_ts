8da0705b5b2d9c4f0db7a837d9cc64595177b50f
tkdlrs <tkd.lsanchez@gmail.com>
2025-12-24 08:50:20 -0700

    My approach and solution to 'CH 1: REPL | L4: REPL' of Boot.dev's 'Build a Pokedex in TypeScript' guided project.

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,30 @@
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process'
+
+const r1 = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+})
+
 export function cleanInput(input: string): string[] {
     return input
         .toLowerCase()
         .trim()
         .split(" ")
         .filter((word) => word != "");
+}
+
+
+export function startREPL() {
+    const display = r1.prompt();
+    r1.on("line", (rawInput: string) => {
+        if (rawInput === "") {
+            r1.prompt();
+        }
+        //
+        console.log(`Your command was: ${cleanInput(rawInput)[0]}`)
+        r1.prompt();
+    })
+    //
 }
